68bd14ed75db1e0f6d5c7236fe771ecca5d38d03
rohitison <rohitbhattthb@gmail.com>
2026-09-05 16:29:17 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -7,4 +7,9 @@ resource "aws_instance" "example" {
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
     associate_public_ip_address = true
+
+    tags = {
+    Name = var.instance_name
+     }
+
 }
